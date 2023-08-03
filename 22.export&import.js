2564d62057d67a636f4160305caa60326f11b993
Hossain Rubel <85532874+rubel703@users.noreply.github.com>
2023-08-03 21:15:46 +0600

    Add files via upload

---
+++ 22.export&import.js
@@ -0,0 +1,3 @@
+import { text,sum } from './22.myModule.js';
+console.log(text);
+console.log(sum(10,20));
---
+++ 22.myModule.js
@@ -0,0 +1,5 @@
+export let text="welcome to module"; 
+
+export let sum=(num1,num2)=>{
+    return num1+num2;
+}
